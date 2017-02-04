5fd0895ad71d48e2c8403aca9d42a2c6e282bc21
acezaga <isaiahtaguibao@yahoo.com>
2017-02-05 04:28:59 +0800

    changes in mongo environment

--- bin/mongoose.js
+++ bin/mongoose.js
@@ -12,5 +12,15 @@ require('../models/Summary');
 require('../models/Performance');
 require('../models/FAQ');
 
+var connection_string = 'mongodb://127.0.0.1/app4db';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1/app4db');
+mongoose.connect(connection_string);
