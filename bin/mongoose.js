88e1cca9e0439ebd98578e518eab6e95b2b659ea
acezaga <isaiahtaguibao@yahoo.com>
2017-02-05 05:14:55 +0800

    changes on connection issues

--- bin/mongoose.js
+++ bin/mongoose.js
@@ -12,12 +12,13 @@ require('../models/Summary');
 require('../models/Performance');
 require('../models/FAQ');
 
-var url = '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
-
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    url = process.env.OPENSHIFT_MONGODB_DB_URL +
+var url = process.env.OPENSHIFT_MONGODB_DB_URL +
     process.env.OPENSHIFT_APP_NAME;
-}
+
+// if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+//     url = process.env.OPENSHIFT_MONGODB_DB_URL +
+//     process.env.OPENSHIFT_APP_NAME;
+// }
 
 mongoose.Promise = global.Promise;
 var connect = function () {
