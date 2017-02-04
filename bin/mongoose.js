3c80cd96f67b9661e1cbd52ae653e596155b5fd9
acezaga <isaiahtaguibao@yahoo.com>
2017-02-05 05:36:06 +0800

    env variables issues on connection

--- bin/mongoose.js
+++ bin/mongoose.js
@@ -12,8 +12,11 @@ require('../models/Summary');
 require('../models/Performance');
 require('../models/FAQ');
 
-var url = process.env.OPENSHIFT_MONGODB_DB_URL +
-    process.env.OPENSHIFT_APP_NAME;
+var url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
 
 // if (process.env.OPENSHIFT_MONGODB_DB_URL) {
 //     url = process.env.OPENSHIFT_MONGODB_DB_URL +
